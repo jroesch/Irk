ec767407c2916a42c47bff8ef74a95ae02f9801d
Tristan Konolige <tristan.konolige@gmail.com>
2013-01-13 01:26:55 -0800

    put connection with server

--- Irk/Server.hs
+++ Irk/Server.hs
@@ -14,9 +14,6 @@ import Control.Proxy hiding (Server)
 import Control.Proxy.Attoparsec
 import Control.Proxy.TChan
 
-import Control.Concurrent.STM
-import Control.Concurrent.STM.TChan
-import Control.Concurrent
 import System.Environment
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
@@ -28,8 +25,25 @@ import Data.Text (Text, unpack, pack)
 import Control.Monad (forever)
 import Control.Exception
 
+import Network (listenOn, PortID(..))
+import Network.BSD
+import Network.Socket hiding (recv)
+import Network.Socket.ByteString (sendAll, recv)
+
+import Control.Proxy hiding (Server)
+import qualified Control.Proxy.Network as N
+import qualified Control.Proxy.Network.Lazy as NL
+import Control.Proxy.TChan
+
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TChan
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC (unpack, pack)
+
 import Irk.Message
-import Irk.Connection
 
 
 filterMessages :: Proxy p => Server -> () -> Pipe p Message Message IO ()
@@ -67,12 +81,12 @@ run server = do
     sock <- listenOn $ PortNumber $ fromIntegral $ port server 
     forever $ do
       (s, addr) <- accept sock
-      runConnection (sendChan server) (recvChan server) (connectionFromSocket s)
+      runConnection server (connectionFromSocket s)
     return ()
 
 connectToPeer :: Server -> ByteString -> Int -> IO ()
 connectToPeer server host port = do
-  connectTo host port >>= runConnection (sendChan server) (recvChan server)
+  connectTo host port >>= runConnection server
   return ()
 
 sendMessage :: Server -> Text -> IO ()
@@ -82,3 +96,56 @@ sendMessage server msg = do
     writeTVar (currentStamp server) $ i + 1
     return $ i + 1
   atomically $ writeTChan (sendChan server) Message{payload=msg, stamp=i}
+
+
+
+-- | Connections
+
+-- | A @Connection@ between two peers
+data Connection = Connection {
+  rawSocket :: Socket
+}
+
+-- | Create a @Connection@ from a @Socket@. The @Socket@ should be ready to
+-- read and write
+connectionFromSocket :: Socket -> Connection
+connectionFromSocket s = Connection { rawSocket = s }
+
+-- Should have a better name
+-- | Runs a @Connection@. New threads are created, so this does not block
+runConnection :: Server -> Connection -> IO ()
+runConnection server conn = do
+    runProxy $ N.sourceSocket (rawSocket conn) >-> toMessageD >-> onConnect server >-> fromMessageD >-> NL.sinkSocket (rawSocket conn)
+    st <- forkIO (sendConn conn server)
+    rt <- forkFinally (recvConn conn server) (\_ -> killThread st)
+    return ()
+
+-- creates a socket for a given hostname and portnumber
+getSocket :: ByteString -> Int -> IO Socket
+getSocket host port = do
+    sock <- socket AF_INET Stream defaultProtocol
+    addr <- inet_addr $ BC.unpack host
+    connect sock $ SockAddrInet (fromIntegral port) addr
+    return sock
+
+connectTo :: ByteString -> Int -> IO Connection
+connectTo host port = do
+    s <- getSocket host port
+    return $ connectionFromSocket s
+
+recvConn :: Connection -> Server -> IO ()
+recvConn conn server = do
+    runProxy $ N.sourceSocket (rawSocket conn) >-> toMessageD >-> sinkTChan (recvChan server)
+    putStrLn "Connection closed"
+
+sendConn :: Connection -> Server -> IO ()
+sendConn conn server = do
+    chan2 <- atomically $ dupTChan $ sendChan server
+    runProxy $ sourceTChan chan2 >-> fromMessageD >-> NL.sinkSocket (rawSocket conn)
+
+onConnect :: Proxy p => Server -> () -> Pipe p Message Message IO ()
+onConnect server () = runIdentityP $ do
+    s <- lift $ atomically $ readTVar $ currentStamp server
+    respond $ Message { stamp = s, payload = pack "Hello" }
+    m <- request ()
+    lift $ atomically $ modifyTVar (currentStamp server) (max (stamp m))
