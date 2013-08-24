1c57a9fa4d1d3ad74d6fe89bb27a8ff69bdad7eb
Jared Roesch <roeschinc@gmail.com>
2013-08-24 02:28:37 -0700

    implement basic echo web socket server

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad
+import Network.WebSockets
+
 main :: IO ()
-main = putStrLn "Running"
+main = runServer "0.0.0.0" 9000 application
+
+application :: Request -> WebSockets Hybi00 ()
+application rq = do
+  acceptRequest rq
+  forever $ do 
+    msg <- receive
+    send msg
+
